f0415d610e3edf4af67ca0bdc38e9eb9c642a396
xebec19 <rohant946@gmail.com>
2024-08-31 22:57:01 +0530

    feat: added floor and gravity

--- src/main.js
+++ src/main.js
@@ -1,13 +1,44 @@
 import kaplay from "kaplay";
 import "kaplay/global";
 
-const k = kaplay()
+const k = kaplay();
 
-k.loadSprite("bean", "sprites/bean.png")
+const SPEED = 320;
+
+k.loadSprite("player", "sprites/ball2.png");
+k.loadSprite("enemy", "sprites/ball3.png");
+k.loadSprite("floor", "sprites/floor.png");
+
+setGravity(1600);
+
+const player = k.add([sprite("player"), pos(3), area(), body()]);
+
+for (let i = 0; i < 3; i++) {
+  const x = rand(0, width());
+
+  k.add([sprite("enemy"), pos(x), area(), body()]);
+}
+
+onKeyDown("left", () => {
+  player.move(-SPEED, 0);
+});
+
+onKeyDown("right", () => {
+  player.move(SPEED, 0);
+});
+
+onKeyPress("space", () => {
+  if (player.isGrounded) {
+    player.jump();
+  }
+});
 
 k.add([
-	k.pos(120, 80),
-	k.sprite("bean"),
-])
-
-k.onClick(() => k.addKaboom(k.mousePos()))
+  rect(width(), 48),
+  outline(4),
+  area(),
+  pos(0, height() - 48),
+  body({
+    isStatic: true,
+  }),
+]);
