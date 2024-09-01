e7756b05e1e4dc8ee41b38ccfb7adbed23a73e91
xebec19 <rohant946@gmail.com>
2024-09-01 15:19:33 +0530

    feat: added gameover

--- src/main.js
+++ src/main.js
@@ -3,42 +3,68 @@ import "kaplay/global";
 
 const k = kaplay();
 
+setBackground(141, 183, 255);
+
 const SPEED = 320;
 
 k.loadSprite("player", "sprites/ball2.png");
 k.loadSprite("enemy", "sprites/ball3.png");
 k.loadSprite("floor", "sprites/floor.png");
 
-setGravity(1600);
+function gameOver() {
+  k.add([text("Game Over", 32), pos(width() / 2, height() / 2)]);
 
-const player = k.add([sprite("player"), pos(3), area(), body()]);
-
-for (let i = 0; i < 3; i++) {
-  const x = rand(0, width());
-
-  k.add([sprite("enemy"), pos(x), area(), body()]);
+  setTimeout(() => {
+    go("game");
+  }, 4000);
 }
 
-onKeyDown("left", () => {
-  player.move(-SPEED, 0);
-});
+scene("game", () => {
+  setGravity(1600);
 
-onKeyDown("right", () => {
-  player.move(SPEED, 0);
-});
+  const player = k.add([sprite("player"), pos(3), area(), body()]);
 
-onKeyPress("space", () => {
-  if (player.isGrounded()) {
-    player.jump();
+  for (let i = 0; i < 3; i++) {
+    const x = rand(0, width());
+
+    k.add([sprite("enemy"), pos(x), area(), body(), "enemy"]);
   }
+
+  onKeyDown("left", () => {
+    if (player.pos.x < 3) {
+      return;
+    }
+    player.move(-SPEED, 0);
+  });
+
+  onKeyDown("right", () => {
+    if (player.pos.x > width() - 100) {
+      return;
+    }
+    player.move(SPEED, 0);
+  });
+
+  onKeyPress("space", () => {
+    if (player.isGrounded()) {
+      player.jump();
+    }
+  });
+
+  k.add([
+    rect(width(), 48),
+    outline(4),
+    area(),
+    pos(0, height() - 48),
+    body({
+      isStatic: true,
+    }),
+  ]);
+
+  player.onCollide("enemy", (enemy) => {
+    addKaboom(player.pos);
+    destroy(player);
+    gameOver();
+  });
 });
 
-k.add([
-  rect(width(), 48),
-  outline(4),
-  area(),
-  pos(0, height() - 48),
-  body({
-    isStatic: true,
-  }),
-]);
+go("game");
